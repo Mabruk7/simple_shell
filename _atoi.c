b7ca0c2c0eab90c8d83044578022c4ebc67357fe
mabruk7 <mabruk33@gmail.com>
2023-07-28 20:29:32 -0700

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -47,15 +47,15 @@ int _isalpha(int c)
 
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
+	int a, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (a = 0; s[a] != '\0' && flag != 2; i++)
 	{
-		if (s[i] == '-')
+		if (s[a] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[a] >= '0' && s[a] <= '9')
 		{
 			flag = 1;
 			result *= 10;
